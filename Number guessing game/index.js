41071c7a58d0ec0a468a7cc7e58ac31c4f23f97a
Hamza Islam <starhax4@gmail.com>
2024-06-29 10:35:36 +0500

    update pkg lock file

---
+++ Number guessing game/index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const generateRandomNumber = () => {
+    let rand = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+    return rand;
+};
+const randomNumber = generateRandomNumber();
+const answer = await inquirer.prompt([
+    { "message": "Enter a guessed Number :", "type": "number", "name": "userNumber" }
+]);
+if (answer.userNumber === randomNumber) {
+    console.log(chalk.green(`You were right, the number was:${randomNumber}`));
+}
+else {
+    console.log(chalk.red(`you were wrong, the number is ${randomNumber}`));
+}
